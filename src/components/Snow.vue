03b0e4b58dc0c3c6c239aec50fef149cac604602
sssejjx <sssejjx@gmail.com>
2025-01-13 03:03:02 +0300

    snow management menu

--- src/components/Snow.vue
+++ src/components/Snow.vue
@@ -2,23 +2,50 @@
   <div class="snow-wrapper">
     <div class="snow-block" ref="snowflake_block"></div>
     <div class="snow-parameters">
+
       <div class="snow-parameters__titel">
-        <span>управление снегом</span>
-        <div class="snow-parameters__open"></div>
-        
+        <span>Управление снегом</span>
+        <div class="snow-parameters__open" @click="showSnowSettings">
+
+          <img src="@/assets/icon/chevron-down.svg" alt="">
+        </div>
       </div>
       
-      <div class="snow-parameters__remove-wrapper">
-        <div class="snow-parameters__remove">
-          <input type="checkbox" id="switch" class="snow-parameters__switch" v-model="snowConditions" />
-          <label for="switch" class="snow-parameters__switch-description">
-          </label>
+      <div class="snow-parameters__wrapper" ref="parameters_wrapper">
+        <div class="snow-parameters__remove-wrapper">
+
+          <div class="snow-parameters__remove">
+            <input type="checkbox" id="switch" class="snow-parameters__switch" v-model="snowConditions" />
+            <label for="switch" class="snow-parameters__switch-description">
+            </label>
+          </div>
+          <div class="snow-parameters__text">
+            <span>Вкл. / Вык.</span>
+          </div>
         </div>
-        <div class="snow-parameters__text">
-          <span>включить/выключить</span>
+
+        <div class="snow-parameters__block">
+          <div class="snow-parameters__text">
+            <label for="numberSnowflakes">Количество снежинок</label>
+          </div>
+          <div class="snow-parameters__amount-snow">
+            <input type="range" id="numberSnowflakes" min="0" max="99" v-model="snowMax" class="snow-parameters__input">
+            <output class="snow-parameters__value" name="value" for="range">{{ +snowMax + 1}}</output>
+          </div>
         </div>
-        
+
+        <div class="snow-parameters__block">
+          <div class="snow-parameters__text">
+            <label for="speedOfFall">Скорость снижения</label>
+          </div>
+          <!-- v-model="minSpeedOfFall" -->
+          <div class="snow-parameters__amount-snow">
+            <input type="range" id="speedOfFall" min="100" max="0"  class="snow-parameters__input">
+            <output class="snow-parameters__value" name="value" for="range">{{ 151 - speedOfFall }}</output>
+          </div>
+        </div>
       </div>
+      
     </div>
     <!-- <span class='snowflake' style='position:absolute;top:10px;'>
       <img src='@/assets/snow/snowflake1.png' alt='#'>
@@ -37,14 +64,32 @@ export default {
       x_mv: [],
       crds: [],
       lftrght: [],
-      snowMax: 100,
+      snowMax: 99,
       snowLocal: [],
       snowMaxSize: 30,
       actualMarginBottom: 0,
       snowConditions: false,
+      showSettings: false,
+      minSpeedOfFall: 1,
+      speedOfFall: 150,
     }
   },
   methods: {
+    stopSetTimeout: function () {
+      const highestTimeoutId = setTimeout(";");
+      for (var i = 0 ; i < highestTimeoutId ; i++) {
+          clearTimeout(i); 
+      }
+    },
+    showSnowSettings: function () {
+      this.showSettings = !this.showSettings;
+      if (this.showSettings) {
+        this.$refs.parameters_wrapper.classList.add('snow-parameters__wrapper-active');
+      } else {
+        this.$refs.parameters_wrapper.classList.remove('snow-parameters__wrapper-active');
+      }
+      
+    },
     randommaker: function (range) {
       return Math.floor(range * Math.random())
     },
@@ -59,7 +104,6 @@ export default {
       const ie5 = document.all && document.getElementById && !browserinfos.match(/Opera/);
       const snowflakes = new Array('snowflake1.png', 'snowflake2.png', 'snowflake3.png');
 
-      // console.log(marginBottom);
       if (ie5 || opera) {
         marginBottom = document.body.clientHeight;
         // this.marginRight = document.body.clientWidth;
@@ -71,7 +115,6 @@ export default {
       
       let snowsizerange = this.snowMaxSize - snowminsize;
 
-      // console.log(marginBottom);
       this.actualMarginBottom = marginBottom;
       for (let i = 0; i <= this.snowMax; i++) {
         this.crds[i] = 0;
@@ -83,7 +126,6 @@ export default {
         // snow[i].style.fontFamily=snowtype[randommaker(snowtype.length)];
         this.snowLocal[i].size = this.randommaker(snowsizerange) + snowminsize;
         // const snowflakeSize = randommaker(snowsizerange) + snowminsize;
-        // console.log(`${snowflakeSize}px`);
 
         this.snowLocal[i].sink = sinkSpeed * this.snowLocal[i].size/5;
         this.snowLocal[i].posx = this.randommaker(marginRight - this.snowLocal[i].size);
@@ -98,9 +140,9 @@ export default {
         this.snowLocal[i].childNodes[0].src = `http://www.s-sta.online/snow/${snowflakes[this.randommaker(snowflakes.length)]}`;
       }
 
-      this.movesnow();
+      this.moveSnow();
     },
-    movesnow: function () {
+    moveSnow: function () {
       const marginRight = this.$refs.snowflake_block.offsetWidth;
 
       for (let i = 0; i <= this.snowMax; i++) {
@@ -117,21 +159,20 @@ export default {
         }
       }
 
-      // console.log(this.snow);
-      setTimeout(this.movesnow, 150);
+      this.stopSetTimeout();
+      setTimeout (this.moveSnow, this.speedOfFall);
     },
     snowToPage: function () {
       for (let i = 0; i <= this.snowMax; i++) {
         this.$refs.snowflake_block.insertAdjacentHTML('afterbegin', "<span id='s" + i + "' class='snow-block__snowflake' style='position:absolute;top:-" + this.snowMaxSize + "'>" + "<img alt='#'></span>");
       }
-    }
+    },
   },
   watch: {
     snowConditions: function () {
       if (this.snowConditions) {
         const allSnowflakes = document.querySelectorAll('div.snow-block > span');
 
-        console.log(allSnowflakes.length);
         for (let i = 0; i <= allSnowflakes.length; i++) {
           if (allSnowflakes[i] !== undefined) {
             allSnowflakes[i].remove();
@@ -141,6 +182,22 @@ export default {
         this.snowToPage();
         this.initsnow();
       }
+    },
+    snowMax: function () {
+      const allSnow = document.querySelectorAll('.snow-block__snowflake');
+
+      if (!this.snowConditions) {
+        for (let i = 0; i < allSnow.length; i++) {
+          if (allSnow[i] !== undefined) {
+            allSnow[i].remove();
+          }
+        }
+        this.snowToPage();
+        this.initsnow();
+      }
+    },
+    speedOfFall: function () {
+      console.log(this.speedOfFall);
     }
   },
   mounted() {
@@ -176,10 +233,10 @@ export default {
 
   .snow-parameters{
     position: fixed;
-    width: 200px;
+    width: 260px;
     height: auto;
     right: 50px;
-    top: 70%;
+    top: 65%;
     z-index: 99999999;
     @media (max-width: 867px) {
       right: 20px;
@@ -187,14 +244,46 @@ export default {
 
     &__titel{
       width: 100%;
-      text-align: center;
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+      background-color: #00000079;
+      padding: 16px 16px;
     }
 
     &__titel>span{
       text-align: center;
       color: #fff;
       font-size: 0.954rem;
+      text-transform: uppercase;
       font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
+
+    &__wrapper{
+      // visibility: hidden;
+      background-color: #000000;
+      padding: 16px 16px;
+      border-radius: 5px;
+      border: 2px solid #151515;
+      opacity: .0;
+      transition: opacity 3s;
+      -webkit-transition: opacity 3s;
+    }
+
+    &__wrapper-active{
+      // visibility: inherit;
+      opacity: 1;
+    }
+
+    &__open{
+      width: 20px;
+      height: 20px;
+      cursor: pointer;
+    }
+
+    &__open>img{
+      width: 20px;
+      height: 20px;
     }
 
     &__remove-wrapper{
@@ -202,6 +291,39 @@ export default {
       display: flex;
       justify-content: space-between;
       align-items: center;
+    }
+
+    &__remove{
+      width: auto;
+      height: 100%;
+      display: flex;
+      align-items: center;
+    }
+
+    &__block{
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      flex-direction: column;
+      margin-top: 15px;
+    }
+
+    &__text{
+      width: auto;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+
+    &__text>span{
+      color: #fff;
+      font-size: 0.85rem;
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+    }
+
+    &__text>label{
       color: #fff;
       font-size: 0.85rem;
       font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -249,6 +371,45 @@ export default {
       width: 90px;
     }
 
+    &__amount-snow{
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+
+    &__input{
+      width: 70%;
+      appearance: none;
+      -webkit-appearance: none;
+      height: 10px;
+      border-radius: 15px; 
+      background: #797979;
+      outline: none;
+      opacity: 0.7;
+      -webkit-transition: .2s;
+      transition: opacity .2s;
+    }
+
+    &__input:hover {
+      opacity: 1; /* Fully shown on mouse-over */
+    }
+
+    &__input::-webkit-slider-thumb{
+      -webkit-appearance: none;
+      appearance: none;
+      width: 25px;
+      height: 25px;
+      border-radius: 50%; 
+      background: #2196F3;
+      cursor: pointer;
+    }
+
+    &__value{
+      width: 23%;
+      text-align: center;
+      border: 1px solid #FCC02C;
+    }
   }
 
 
